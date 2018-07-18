00e24f0585fa16f67a23e3df26d4049e18f70f55
takecian <takecian@gmail.com>
2018-07-18 14:31:25 +0900

    Add sample app written in Objc

---
+++ DemoObjc/DemoObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoObjc
+//
+//  Created by Fujiki Takeshi on 2018/07/18.
+//  Copyright © 2018 takecian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
