243ad8de8d7f59f43302d4f98f6bd2a28ff24756
Takeshi Fujiki <takecian@gmail.com>
2024-11-03 23:58:34 +0900

    Update versions

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_10)
+        .macOS(.v10_15)
     ],
     products: [
         .library(
